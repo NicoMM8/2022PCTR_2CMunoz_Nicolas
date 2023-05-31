63dbc9e358ac3f4e61e8c071dfb47241c2172f1f
nicom <nicom@DESKTOP-126EM4C>
2023-05-31 17:45:29 +0200

    Terminado el código base

--- 2022PCTR_2CMunoz_Nicolas/ActividadAliada.java
+++ 2022PCTR_2CMunoz_Nicolas/ActividadAliada.java
@@ -1,1 +1,26 @@
-//Hola
+
+
+
+
+public class ActividadAliada implements Runnable {
+	private  final Juego juego;
+	private final tipoEnemigo;
+	
+	public ActividadAliada(Juego juego, int tipoEnemigo) {
+		this.juego = juego;
+		this.tipoEnemigo = tipoEnemigo;
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+		juego.eliminarEnemigo(tipoEnemigo);
+		try {
+			Thread.sleep(new Random().nextInt(5000) + 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
+}
