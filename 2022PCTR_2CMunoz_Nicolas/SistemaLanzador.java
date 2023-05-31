4158aa8355ac29eea647036248d89bbe5c38dfd3
nicom <nicom@DESKTOP-126EM4C>
2023-05-31 19:40:32 +0200

    SistemaLanzador completado.

---
+++ 2022PCTR_2CMunoz_Nicolas/SistemaLanzador.java
@@ -0,0 +1,24 @@
+/**
+ *
+ *Clase que contiene el método main para lanzar la simulación del juego.
+ *
+ * @author Nicolás Muñoz
+ */
+
+public class SistemaLanzador {
+	public static void main(String[] args) {
+		int numTiposEnemigos = 3; //Número de tipos de enemigos
+		int numEnemigosPorTipo = 5; //Número de enemigos por tipo
+		int maxEnemigosSimultaneos = 2; //Número máximo de enemigos simultáneos
+		
+		Juego juego = new Juego(numTiposEnemigos, numEnemigosPorTipo, numEnemigosSimultaneos);
+		
+		//Generar hilos de actividad enemiga y actividad aliada para cada tipo de enemigo
+		int (int i = 1; i <= numTiposEnemigos; i++) {
+			Thread enemigoThread = new Thread(new ActividadEnemiga(juego, i));
+			Thread aliadoThread = new Thread(new ActividadAliada(juego, i));
+			enemigoThread.start();
+			aliadoThread.start();
+		}
+	}
+}
