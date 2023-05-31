4ae01ea323fef04e20ada0b88343b3380cc3661e
nicom <nicom@DESKTOP-126EM4C>
2023-05-31 18:00:21 +0200

    Completados los comentarios de ActividadAliada.java

--- 2022PCTR_2CMunoz_Nicolas/ActividadAliada.java
+++ 2022PCTR_2CMunoz_Nicolas/ActividadAliada.java
@@ -1,6 +1,11 @@
-
-
-
+/**
+ *
+ * Representa la ctividad aliada en el juego.
+ * Esta clase implementa la interfaz Runnable y se utiliza para crear hilos que
+ * eliminan enemigos de un tipo específico.
+ *
+ *@author Nicolás Muñoz
+ */
 
 public class ActividadAliada implements Runnable {
 	private  final Juego juego;
@@ -11,11 +16,19 @@ public class ActividadAliada implements Runnable {
 		this.tipoEnemigo = tipoEnemigo;
 	}
 	
+	/**
+	 * Método run que se ejecuta cuando inicia el hilo de la actividad aliada.
+	 * Se intenta eliminar enemigos del tipo especificado en un bucle infinito.
+	 * Después de cada eliminación, el hilo se duerme 
+	 * durante un tiempo aleatorio antes de intentar eliminar otro enemigo
+	 */
+	
 	@Override
 	public void run() {
 		while (true) {
 		juego.eliminarEnemigo(tipoEnemigo);
 		try {
+			// Esperar un tiempo aleatorio antes de intentar eliminar otro enemigo
 			Thread.sleep(new Random().nextInt(5000) + 1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
